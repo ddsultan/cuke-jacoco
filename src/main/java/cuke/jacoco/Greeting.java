4a04f2c29894cbb265238cd0dea3e37a14f962a5
Dilshod Sultonov <dsul@camelot-itlab.com>
2020-12-22 23:35:00 +0100

    refactor: move greet logic to its own class

---
+++ src/main/java/cuke/jacoco/Greeting.java
@@ -0,0 +1,7 @@
+package cuke.jacoco;
+
+public class Greeting {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+}
--- src/test/java/cuke/jacoco/AppTest.java
+++ src/test/java/cuke/jacoco/AppTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
+        var classUnderTest = new Greeting();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
